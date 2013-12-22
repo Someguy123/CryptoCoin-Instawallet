359f6c8e71e026e6985bcba33f46d961f1a121be
blixnood <blixnood-github.com@lurkmore.com>
2013-12-22 04:55:33 -0500

    Fix $sitename

--- templates/header.php
+++ templates/header.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html><head>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title><?php echo $sitetitle; ?></title>
+  <title><?php echo $sitename; ?></title>
   <link rel="stylesheet" href="/assets/bootstrap.min.css">
   <link rel="stylesheet" href="/assets/bootstrap-responsive.css">
   <link rel="stylesheet" href="/assets/style.css">
